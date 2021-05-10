f986c1d3a6134a7a36892b6ffaa891732b095f8a
ghd0276 <ghd0276@naver.com>
2021-05-10 17:09:10 +0900

    5/10 복습

--- 12Chapter/quiz.c
+++ 12Chapter/quiz.c
@@ -10,6 +10,7 @@ int main()
 	int size = sizeof(lotto) / sizeof(lotto[0]);
 
 	input_lotto(lotto, size);
+	printf("\n");
 	print_lotto(lotto, size);
 
 	return 0;
@@ -22,10 +23,10 @@ void input_lotto(int* pl, int size)
 	int j;
 	int temp;
 
-	printf("로또 번호 입력 : \n");
 
 	while (i < size)
 	{
+		printf("로또 번호 입력 : ");
 		scanf("%d", &temp);
 		if (i == 0)
 		{
@@ -38,14 +39,14 @@ void input_lotto(int* pl, int size)
 				if (temp == pl[j])
 				{
 					printf("중복된 숫자를 입력하지 마시오.\n");
-					flag = -1;
-					break;
+					flag = -1; // 중복된 숫자가 있다는 플래그
+					break; // for문을 탈출하여 해당 순서의 번호 입력으로 다시 돌아감
 				}
-				flag = 1;
+				flag = 1; // 중복된 숫자가 없다는 플래그
 			}
 		}
 
-		if (flag > 0)
+		if (flag > 0) // 중복된 숫자 없는경우 로또 배열에 번호 입력
 		{
 			*(pl + i) = temp;
 			i++;
