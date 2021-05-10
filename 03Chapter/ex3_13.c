3a2fe33ca86fbf5ed1028c20b928b58045f5b17e
ghd0276 <ghd0276@naver.com>
2021-05-10 14:13:31 +0900

    이미지 업로드

--- 03Chapter/ex3_13.c
+++ 03Chapter/ex3_13.c
@@ -9,9 +9,9 @@ int main(void)
 	//정수 다음 문자를 입력받는경우
 	printf("정수 하나를 입력하세요 : ");
 	scanf("%d", &a);
-	getchar(); // 버퍼에 있는 문자하나를 불러옴
+	//getchar(); // 버퍼에 있는 문자하나를 불러옴
 	printf("문자 하나를 입력하세요 : ");
-	scanf("%c", &b); // getchar안쓰고 " %c"써도 가능
+	scanf(" %c", &b); // getchar안쓰고 " %c"써도 가능
 
 	printf("입력된 값은 %d, %c 입니다.\n", a, b);
 	
